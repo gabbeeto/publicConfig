1e01ea88cc144968e7cf7c1dbae6ed3c5a90ed0a
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-05-28 09:40:30 -0300

    add git config

--- gitConfig.sh
+++ gitConfig.sh
@@ -5,3 +5,10 @@ git config --global init.defaultBranch main
 git config --global pull.rebase false
 git config --global core.editor "hx"
 
+# generate key for github
+ssh-keygen -t ed25519
+
+# shows key
+cat ~/.ssh/id_ed25519.pub
+# opens firefox in this website so I can add my key
+firefox https://github.com/settings/keys
