7b9edc6a141f65d8fe8e3b147811965adf7704fa
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-06-01 00:44:41 -0300

    fix brb

--- .config/i3/brbSwitch.sh
+++ .config/i3/brbSwitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-feh --fullscreen ~/.config/i3/assets/brb.png
+feh --scale-down  ~/.config/i3/assets/brb.png
 bash ~/.config/i3/toggleMic.sh
 bash ~/.config/i3/moveChatToWorkspace.sh
 killall polybar
