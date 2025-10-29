03f471371c9d76e1789de6430fe41cb44a6cc697
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-10-29 15:11:02 -0300

    remove duplicates lol

--- installStuff.sh
+++ installStuff.sh
@@ -2,109 +2,95 @@
 sudo pacman -Syu --noconfirm
 
 # niri and wayland
-sudo pacman -S niri --noconfirm -needed
-sudo pacman -S wayland --noconfirm -needed
-sudo pacman -S wayland-utils wayland-protocols --noconfirm -needed
-sudo pacman -S libinput mesa vulkan-icd-loader --noconfirm -needed
+sudo pacman -S niri --noconfirm --needed
+sudo pacman -S wayland --noconfirm --needed
+sudo pacman -S wayland-utils wayland-protocols --noconfirm --needed
+sudo pacman -S libinput mesa vulkan-icd-loader --noconfirm --needed
 # download firefox
-sudo pacman -S firefox chromium  --noconfirm -needed
+sudo pacman -S firefox chromium  --noconfirm --needed
 
 # I like this window manager lol
-sudo pacman -S nautilus -needed
+sudo pacman -S nautilus --needed
 
 
 # for when firefox cannot handle 360p and you're in a very old devie
-sudo pacman -S yt-dlp --noconfirm -needed
+sudo pacman -S yt-dlp --noconfirm --needed
 
 # bar on top
-sudo pacman -S waybar --noconfirm -needed
+sudo pacman -S waybar --noconfirm --needed
 
 # waybar fonts
-sudo pacman -S ttf-font-awesome --noconfirm -needed
-sudo pacman -S ttf-nerd-fonts-symbols --noconfirm -needed
-sudo pacman -S ttf-roboto-mono noto-fonts --noconfirm -needed
-sudo pacman -S otf-font-awesome --noconfirm -needed
+sudo pacman -S ttf-font-awesome --noconfirm --needed
+sudo pacman -S ttf-nerd-fonts-symbols --noconfirm --needed
+sudo pacman -S ttf-roboto-mono noto-fonts --noconfirm --needed
+sudo pacman -S otf-font-awesome --noconfirm --needed
 
-sudo pacman -S noto-fonts-emoji --noconfirm -needed
+sudo pacman -S noto-fonts-emoji --noconfirm --needed
 
 # app to search on niri
-sudo pacman -S fuzzel --noconfirm -needed
+sudo pacman -S fuzzel --noconfirm --needed
 
 
 # manage x11 apps
-sudo pacman -S xwayland-satellite --noconfirm -needed
+sudo pacman -S xwayland-satellite --noconfirm --needed
 
 # to copy key ssh key
-sudo pacman -S wl-clipboard --noconfirm -needed
+sudo pacman -S wl-clipboard --noconfirm --needed
 
-# niri and wayland
-sudo pacman -S niri --noconfirm -needed
-sudo pacman -S wayland --noconfirm -needed
-sudo pacman -S wayland-utils wayland-protocols --noconfirm -needed
-sudo pacman -S libinput mesa vulkan-icd-loader --noconfirm -needed
-# download firefox
-sudo pacman -S firefox chromium  --noconfirm -needed
-
-# I like this window manager lol
-sudo pacman -S nautilus -needed
-
-
-# for when firefox cannot handle 360p and you're in a very old devie
-sudo pacman -S yt-dlp --noconfirm -needed
 
 # bar on top
-sudo pacman -S waybar --noconfirm -needed
+sudo pacman -S waybar --noconfirm --needed
 
 # waybar fonts
-sudo pacman -S ttf-font-awesome --noconfirm -needed
-sudo pacman -S ttf-nerd-fonts-symbols --noconfirm -needed
-sudo pacman -S ttf-roboto-mono noto-fonts --noconfirm -needed
-sudo pacman -S otf-font-awesome --noconfirm -needed
+sudo pacman -S ttf-font-awesome --noconfirm --needed
+sudo pacman -S ttf-nerd-fonts-symbols --noconfirm --needed
+sudo pacman -S ttf-roboto-mono noto-fonts --noconfirm --needed
+sudo pacman -S otf-font-awesome --noconfirm --needed
 
-sudo pacman -S noto-fonts-emoji --noconfirm -needed
+sudo pacman -S noto-fonts-emoji --noconfirm --needed
 
 # app to search on niri
-sudo pacman -S fuzzel --noconfirm -needed
+sudo pacman -S fuzzel --noconfirm --needed
 
 
 # manage x11 apps
-sudo pacman -S xwayland-satellite --noconfirm -needed
+sudo pacman -S xwayland-satellite --noconfirm --needed
 
 
 # notification
-sudo pacman -S mako --noconfirm -needed
+sudo pacman -S mako --noconfirm --needed
 
 # lock screen
-sudo pacman -S swaylock  --noconfirm -needed
+sudo pacman -S swaylock  --noconfirm --needed
 
 # to install ripdrag
-sudo pacman -Sy --needed rust gtk4 base-devel --noconfirm -needed
+sudo pacman -Sy ---needed rust gtk4 base-devel --noconfirm --needed
 
 
 # best terminal file manager and dependencies
-sudo pacman -S yazi  7zip  poppler fd ripgrep  zoxide resvg imagemagick ttf-nerd-fonts-symbols  --noconfirm -needed
+sudo pacman -S yazi  7zip  poppler fd ripgrep  zoxide resvg imagemagick ttf-nerd-fonts-symbols  --noconfirm --needed
 
 
 # for c++ and godot
-sudo pacman -S clang scons python3 --noconfirm -needed
+sudo pacman -S clang scons python3 --noconfirm --needed
 
 
 # apps for that I use daily
-sudo pacman -S curl kitty unzip obs-studio go helix tmux zellij npm nodejs python-pip python-pipx python discord flatpak fzf arch-wiki-docs jre-openjdk mpv blender pavucontrol wf-recorder man-db man-pages less --noconfirm -needed
+sudo pacman -S curl kitty unzip obs-studio go helix tmux zellij npm nodejs python-pip python-pipx python discord flatpak fzf arch-wiki-docs jre-openjdk mpv blender pavucontrol wf-recorder man-db man-pages less --noconfirm --needed
 
 
 # install gaming stuff
-sudo pacman -S lutris wine-staging winetricks steam gamemode lib32-gamemode --noconfirm -needed
+sudo pacman -S lutris wine-staging winetricks steam gamemode lib32-gamemode --noconfirm --needed
 # install retroarch
-sudo pacman -S retroarch --noconfirm -needed
+sudo pacman -S retroarch --noconfirm --needed
 
 
 
 # dependencies for raylib
-sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama libxkbcommon lib32-wayland lib32-libxkbcommon --noconfirm -needed
+sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama libxkbcommon lib32-wayland lib32-libxkbcommon --noconfirm --needed
 
 
-flatpak-install-if-needed() {
+flatpak-install-if--needed() {
     if ! flatpak list --app | grep -q "$1"; then
         flatpak install --noninteractive flathub "$1"
     else
@@ -113,20 +99,20 @@ flatpak-install-if-needed() {
 }
 
 
-flatpak-install-if-needed org.kde.krita
-flatpak-install-if-needed org.gimp.GIMP
-flatpak-install-if-needed com.orama_interactive.Pixelorama
-flatpak-install-if-needed org.inkscape.Inkscape
-flatpak-install-if-needed com.chatterino.chatterino
-flatpak-install-if-needed org.kde.kdenlive
-flatpak-install-if-needed org.libreoffice.LibreOffice
-flatpak-install-if-needed org.luanti.luanti
-flatpak-install-if-needed org.vinegarhq.Vinegar
-flatpak-install-if-needed org.vinegarhq.Sober 
-flatpak-install-if-needed io.github.jliljebl.Flowblade
+flatpak-install-if--needed org.kde.krita
+flatpak-install-if--needed org.gimp.GIMP
+flatpak-install-if--needed com.orama_interactive.Pixelorama
+flatpak-install-if--needed org.inkscape.Inkscape
+flatpak-install-if--needed com.chatterino.chatterino
+flatpak-install-if--needed org.kde.kdenlive
+flatpak-install-if--needed org.libreoffice.LibreOffice
+flatpak-install-if--needed org.luanti.luanti
+flatpak-install-if--needed org.vinegarhq.Vinegar
+flatpak-install-if--needed org.vinegarhq.Sober 
+flatpak-install-if--needed io.github.jliljebl.Flowblade
 
 
-npm-install-if-needed() {
+npm-install-if--needed() {
     if ! npm list -g "$1" --depth=0 | grep -q "$1"; then
         sudo npm install -g "$1" --no-fund --no-audit
     else
@@ -135,19 +121,19 @@ npm-install-if-needed() {
 }
 
  # install language servers for helix
-npm-install-if-needed vscode-langservers-extracted
-npm-install-if-needed @olrtg/emmet-language-server
-npm-install-if-needed typescript
-npm-install-if-needed typescript-language-server
+npm-install-if--needed vscode-langservers-extracted
+npm-install-if--needed @olrtg/emmet-language-server
+npm-install-if--needed typescript
+npm-install-if--needed typescript-language-server
 
-sudo pacman -S python-lsp-server --noconfirm -needed
+sudo pacman -S python-lsp-server --noconfirm --needed
 pipx install PySide6
 pipx upgrade PySide6
 
 
 
 # for godot language server
-sudo pacman -S nmap -needed --noconfirm
+sudo pacman -S nmap ---needed --noconfirm
 pipx install "gdtoolkit==4.*"
 pipx upgrade "gdtoolkit==4.*"
 
--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
-
 echo "=== Starting Update ==="
 flatpak update
 echo "Updating flatpaks..."
@@ -9,3 +8,7 @@ bash ./installStuff.sh
 echo "Checking and changing folder layout..."
 bash ./structureFolders.sh
 echo "=== Update Complete ==="
+
+# restart waybar
+pkill waybar
+systemctl --user restart waybar
